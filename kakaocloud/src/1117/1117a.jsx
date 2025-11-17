cdacff30368c85e38e4109238efe7c3c0f3c19ba
seongbokim <rkdtlgn0@naver.com>
2025-11-17 11:50:04 +0900

    modified:   kakaocloud/src/1117/1117a.jsx

--- kakaocloud/src/1117/1117a.jsx
+++ kakaocloud/src/1117/1117a.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function Plus() {
   const [a, setA] = useState("");
   const [b, setB] = useState("");
-  const [result, setResult] = useState(0);
+  const [result, setResult] = useState("");
   const onClickEnter = () => {
     setResult(parseInt(a) + parseInt(b));
   };
@@ -11,8 +11,11 @@ function Plus() {
   return (
     <div>
       A : <input type="text" value={a} onChange={(e) => setA(e.target.value)} />
+      <br />
       B : <input type="text" value={b} onChange={(e) => setB(e.target.value)} />
+      <br />
       <button onClick={onClickEnter}>더하기</button>
+      <br />
       a + b = <input value={result} />
     </div>
   );
