9ace97dc752cfdd2b0b7f1d644eeff2eeb55b7e9
seongbokim <rkdtlgn0@naver.com>
2025-11-19 09:02:39 +0900

    modified:   kakaocloud/src/1118/Home.jsx 	new file:   kakaocloud/src/1118/Profile.jsx 	modified:   kakaocloud/src/App.jsx

--- kakaocloud/src/App.jsx
+++ kakaocloud/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import About from "./1118/About.jsx";
 import Home from "./1118/Home.jsx";
+import Profile from "./1118/Profile.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/profiles/:username" element={<Profile />} />
     </Routes>
   );
 }
