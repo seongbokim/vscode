58e670bad357fafed48cfec219dfc82d0d794e0a
seongbokim <rkdtlgn0@naver.com>
2025-11-19 16:31:54 +0900

    modified:   kakaocloud/src/1119/components/ColorBox.jsx 	new file:   kakaocloud/src/1119/components/SelectColors.jsx 	modified:   kakaocloud/src/App.jsx

--- kakaocloud/src/1119/components/ColorBox.jsx
+++ kakaocloud/src/1119/components/ColorBox.jsx
@@ -1,26 +1,24 @@
-import { ColorConsumer } from "../contexts/color";
+import { useContext } from "react";
+import ColorContext, { ColorConsumer } from "../contexts/color";
 
 export default function ColorBox() {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 }
--- kakaocloud/src/App.jsx
+++ kakaocloud/src/App.jsx
@@ -1,10 +1,13 @@
 import ColorBox from "./1119/components/ColorBox.jsx";
 import { ColorProvider } from "./1119/contexts/color.jsx";
+import SelectColors from "./1119/components/SelectColors.jsx";
+import "./App.css";
 
 export default function App() {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
