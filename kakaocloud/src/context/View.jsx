bc66d28fda8b16402b6d42feb7d1e100ac54dbda
seongbokim <rkdtlgn0@naver.com>
2025-11-24 09:06:10 +0900

    modified:   kakaocloud/src/App.jsx 	new file:   kakaocloud/src/App3.jsx 	deleted:    kakaocloud/src/BBS/DataUser.jsx 	new file:   kakaocloud/src/context/DataContext.jsx 	new file:   kakaocloud/src/context/DataUser.jsx 	new file:   kakaocloud/src/context/Home.jsx 	new file:   kakaocloud/src/context/Signup.jsx 	new file:   kakaocloud/src/context/View.jsx 	new file:   kakaocloud/src/context/Write.jsx

---
+++ kakaocloud/src/context/View.jsx
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext.jsx";
+
+export default function View() {
+  const { id } = useParams();
+  const { data } = useContext(DataContext);
+  const views = data.find((x) => x.id === parseInt(id));
+
+  return (
+    <div>
+      <div>
+        <Link to="/home">홈으로</Link>
+      </div>
+      <hr />
+      <h2>게시글 내용</h2>
+      <p>번호 : {views.id} </p>
+      <p>제목 : {views.title} </p>
+      <p>작성자 : {views.user} </p>
+      <p>조회수 : {views.view} </p>
+      <p>좋아요(추천) : {views.like} </p>
+    </div>
+  );
+}
