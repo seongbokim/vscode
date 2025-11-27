6afd25c51bfb9fb4378e4d79eede52c105ba43db
seongbokim <rkdtlgn0@naver.com>
2025-11-27 16:41:20 +0900

    deleted:    kakaopython/1126/__pycache__/memu.cpython-314.pyc

---
+++ kakaopython/1127/cal1127a.py
@@ -0,0 +1,10 @@
+class Calculator:
+    def __init__(self, a, b):
+        self.x = a
+        self.y = b
+
+    def add(self):
+        return self.x + self.y
+    def sub(self):
+        return self.x - self.y
+
